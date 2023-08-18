0abae4ffce5559703b9ea11d02973a26ae61abf2
syy <910427747@qq.com>
2023-08-18 09:53:08 +0800

    feat：图标修改

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -475,7 +475,7 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>ChatGPT Next Web</div>
+            <div className={styles["main-title"]}>影子灰</div>
             <div className={styles["sub-title"]}>
               github.com/Yidadaa/ChatGPT-Next-Web
             </div>
